dc67d8431960a6d06c3249302efd36398483d1b3
liesbeth1987 <r0888657@student.thomasmore.be>
2023-01-06 13:58:21 +0100

    final fixes

--- src/app/dca/dca.page.ts
+++ src/app/dca/dca.page.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import {DcaService} from '../services/dca.service';
-import {ActivatedRoute, Router} from '@angular/router';
 import {AuthService} from '../services/auth.service';
 import {DatabaseService} from '../services/database.service';
 
--- src/app/markets/markets-detail/markets-detail.module.ts
+++ src/app/markets/markets-detail/markets-detail.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { MarketsDetailPageRoutingModule } from './markets-detail-routing.module';
-
 import { MarketsDetailPage } from './markets-detail.page';
 
 @NgModule({
